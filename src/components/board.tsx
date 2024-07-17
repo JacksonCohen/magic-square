128658b211fe664df3f5ec0db5e0c9980aa9e709
Jackson Cohen <jacksonrcohen@gmail.com>
2024-07-17 09:40:31 -0400

    Create board grid

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,3 +1,37 @@
+import { useState } from 'react';
+
+const NUMBERS = [1, 2, 3, 4, 5, 6];
+const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F'];
+
 export default function Board() {
-  return <div>board</div>;
+  const [grid, setGrid] = useState<(string | null)[][]>(new Array(6).fill(new Array(6).fill(null)));
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          {NUMBERS.map((number) => (
+            <th key={number}>{number}</th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {grid.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            <td>{LETTERS[rowIndex]}</td>
+            {row.map((cell, cellIndex) => (
+              <td
+                key={cellIndex}
+                className='[&:not(:first-child)]:border-t border-b border-black border-l last:border-r w-[20px]'
+              >
+                {cell}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
