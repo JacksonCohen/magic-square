cc2c977401739459053235b1db2246d36cadf31d
Jackson Cohen <jacksonrcohen@gmail.com>
2024-07-29 20:42:13 -0400

    Remove unnecessary `Square` prop

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -3,12 +3,10 @@ import invariant from 'tiny-invariant';
 import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
 
 import { type ReactNode } from 'react';
-import { type PieceRecord } from './piece';
 
-export type Coords = number[][] | null;
+export type Coords = number[][] | null; // TODO: Remove null and update name
 
 interface SquareProps {
-  placedPieces: PieceRecord[];
   location: Coords;
   children: ReactNode;
   highlightedSquares: { coords: Coords; valid: boolean }[];
@@ -16,12 +14,7 @@ interface SquareProps {
 
 export type HoveredState = 'idle' | 'validMove' | 'invalidMove';
 
-export default function Square({
-  placedPieces,
-  location,
-  children,
-  highlightedSquares,
-}: SquareProps) {
+export default function Square({ location, children, highlightedSquares }: SquareProps) {
   const ref = useRef(null);
 
   const isHighlighted = highlightedSquares.some(
@@ -29,7 +22,7 @@ export default function Square({
       highlight.coords?.[0][0] === location?.[0][0] && highlight.coords?.[0][1] === location?.[0][1]
   );
   const validHighlight = highlightedSquares.length > 0 ? highlightedSquares[0].valid : true;
-  const highlightClass = isHighlighted ? (validHighlight ? 'bg-green-300' : 'bg-red-300') : '';
+  const highlightClass = isHighlighted ? (validHighlight ? ' bg-green-300' : ' bg-red-300') : '';
 
   useEffect(() => {
     const el = ref.current;
@@ -39,11 +32,11 @@ export default function Square({
       element: el,
       getData: () => ({ location }),
     });
-  }, [location, placedPieces]);
+  }, [location]);
 
   return (
     <div
-      className={`flex justify-center items-center w-16 h-16 shadow-inner ${highlightClass}`}
+      className={`flex justify-center items-center w-16 h-16 shadow-inner${highlightClass}`}
       ref={ref}
     >
       {children}
